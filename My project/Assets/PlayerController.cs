3cbbbf5cb7d413c8c43f17f4e933ae875d97770d
conner lindow <conner.l446@oldstu.nebo.edu>
2022-09-30 13:23:43 -0600

    box game

---
+++ My project/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        
+        //Moves the player left and right
+        transform.Translate(Vector2.right * hInput * speed Time.)
+    }
+}
